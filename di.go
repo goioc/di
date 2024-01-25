906e16260d785a51e800ccf6e08c9fad48923044
Dmytro Titov <dmytro.titov@gmail.com>
2024-01-25 20:33:04 +0100

    Update year in copyright

--- di.go
+++ di.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Go IoC
+ * Copyright (c) 2024 Go IoC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
